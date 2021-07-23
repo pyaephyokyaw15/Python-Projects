4fc95fbb894df264e5a805cffa716f5af6d2308e
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2021-07-23 10:52:55 +0630

    Updeate files

--- 4.Web Scraping/TIMESJOBS/timesjobs.py
+++ 4.Web Scraping/TIMESJOBS/timesjobs.py
@@ -1,3 +1,4 @@
+
 """
 
 - This script is scrape the TIMESJOBS website, which is job searching website.
@@ -7,8 +8,10 @@
 
 """
 
-# !pip install beautifulsoup4
-# !pip install lxml
+# pip install beautifulsoup4
+# pip install lxml
+
+
 
 # import modules
 from bs4 import BeautifulSoup
@@ -23,8 +26,19 @@ seq = 1   # set page no
 # fetch data until the last page
 while True:
 
-    # create url to fetch data on one page after another page 
-    url = base_url + '/candidate/job-search.html?from=submit&txtKeywords=' + my_job+'&cboWorkExp1='+experience+'&sequence='+str(seq)
+    # create url to fetch data on one page after another page
+    '''
+    Sample URL:
+    https://www.timesjobs.com/candidate/job-search.html?from=submit&actualTxtKeywords=python
+    &searchBy=0&rdoOperator=OR&searchType=personalizedSearch&luceneResultSize=25&
+    postWeek=60&txtKeywords=python&cboWorkExp1=15&pDate=I&sequence=1&startPage=1
+    '''
+    
+    startPage = 1 + ((seq-1)//10)*10  # formula for startPage query
+
+    # generate URL
+    url = base_url + '/candidate/job-search.html?from=submit&txtKeywords=' + my_job+'&cboWorkExp1='+\
+            experience+'&sequence='+str(seq)+'&startPage='+str(startPage)
 
     # fetch HTMl text
     html_text = requests.get(url).text
@@ -35,7 +49,7 @@ while True:
 
     # loop all jobs and dispaly each job details.
     for job in jobs:
-
+        print()
         # find required data in HTML tags
         position = job.find('h2').text.strip()
         print('Position: ', position)
@@ -47,22 +61,25 @@ while True:
         print('Posted Date: ',published_date)
         more_info = job.header.h2.a['href']
         print('More info: ', more_info)
-        print('-----------------------------------------------------------------------')
+        print()
+        print('-----------------------------------------------------------------')
 
     # after one page, move to another page
-    # check the page number bar and find current page
+    # check the page number bar and find current page number
     bar = soup.find('div', class_='srp-pagination clearfix')
-    # print(bar.find('em', class_='active').text)
+
 
     # get all  page numbers on the bar
     sequence_bar = bar.find_all('em')
-    # print(sequence_bar[-1].text)
+
 
     # check the current page is the last page
-    # print(bar.find('em', class_='active').text == sequence_bar[-1].text)
+    is_last_page = bar.find('em', class_='active').text == sequence_bar[-1].text
 
     # if current page is the last page, terminate loop.
     # else, move to another page and fetch data.
-    if bar.find('em', class_='active').text == sequence_bar[-1].text:
+    if is_last_page:
         break
+
+    # increase page number to fectch data    
     seq = seq+1
